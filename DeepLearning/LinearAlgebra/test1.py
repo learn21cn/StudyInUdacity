50b462f173771c64790e13847c3e175a6e3ecf72
learn21cn <learn21cn@yeah.net>
2018-01-30 21:05:15 +0800

    update

--- DeepLearning/LinearAlgebra/test1.py
+++ DeepLearning/LinearAlgebra/test1.py
@@ -1,52 +1,123 @@
-# import math
-# from decimal import Decimal, getcontext
-# from vector import Vector
+def transpose(M):
+    # new_col_count = len(M)
+    print(M)
+    for i in zip(*M):
+        print(i)
+    print(list(zip(*M)))
+    return list(map(list,zip(*M)))
 
-class Test(object):
+# 这个zip在不同版本的不同反应了python的一个演变：python3中大部分返回list的函数不在返回list，而是返回一个支持遍历的对象，比如map、fiter之类
+
+def transpose2(M):
+    for i in range(len(M)):
+        for j in range(i):
+            M[i][j], M[j][i] = M[j][i], M[i][j]
+    return M
+
+
+def multi(M1, M2):
+    if isinstance(M1, (float, int)) and isinstance(M2, (tuple, list)):
+        return [[M1*i for i in j] for j in M2]
+    if isinstance(M1, (tuple, list)) and isinstance(M2, (tuple, list)):
+        return [[sum(map(lambda x: x[0]*x[1], zip(i,j))) for j in zip(*M2)] for i in M1]
+
+
+
+def matrixMul(A, B):
+    res = [[0] * len(B[0]) for i in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                res[i][j] += A[i][k] * B[k][j]
+    return res
+
+def matrixMul2(A, B):
+    return [[sum(a * b for a, b in zip(a, b)) for b in zip(*B)] for a in A]
+
+
+def matxMultiply(A, B):
+    countlist_A = [len(c) for c in A]
+    countset_A = set(countlist_A)
     
-    # BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG = ('The basepoint and direction vectors all live in the same dimension')
-
-    def __init__(self, basepoint, direction_vectors):
-   
+    countlist_B = [len(c) for c in B]
+    countset_B = set(countlist_B)
+    
+    try:
+        assert len(countset_A) ==1 and len(countset_B) ==1 
         
-        self.basepoint = basepoint
-        self.direction_vectors = direction_vectors
-
-        # self.dimension = self.basepoint.dimension 
-
-        # print('=======================')  
-        # print(self.basepoint)
-        print(self.direction_vectors)  
-
-        # print('=======================')     
-
-        # try:
-        #     print(direction_vectors)
-        #     for item in direction_vectors:
-        #         v = Vector(item)
-        #         print(v)                
-        #         assert v.dimension == self.dimension
-        
-        # except AssertionError:
-        #     raise Exception(BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
+        if len(B) == len(A[0]): 
+            # print(list(zip(*B)))
+            print([[ list(zip(a,b)) for b in zip(*B)]  for a in A])  
+            
+            # 这个是错误的结果
+            print([[sum(i*j for i, j in zip(a, b)) for a in A] for b in zip(*B)])
+            # 这个是正确的结果，注意顺序
+            return [[sum(i*j for i, j in zip(a, b)) for b in zip(*B)] for a in A]
+            
+        else:
+            raise ValueError('矩阵A的列数与B的行数不相等，不能相乘')
+    
+    except AssertionError:
+        raise Exception('A和必须是是有效的矩阵')
 
 
-# Vector: (Decimal('-0.3010471533'), Decimal('-0.4919135404'), Decimal('0'))
-# [[Decimal('-0.09084497715'), Decimal('0.5086234347'), 1]]
+a = [[1,2], [3,4], [5,6], [7,8]]
+b = [[1,2,3,4], [5,6,7,8]]
+# print(matrixMul(a,b))
+# print(matrixMul(b,a))
+# print("-"*90)
+# print(matrixMul2(a,b))
+# print(matrixMul2(b,a))
 
+# print("-"*90)
+# print(multi(a, b))
 
-print(12345)
-if __name__ == "__main__":
-    # p = Test(basepoint = Vector([Decimal('-0.3010471533'), Decimal('-0.4919135404'), Decimal('0')]), direction_vectors=[[Decimal('-0.09084497715'), Decimal('0.5086234347'), 1]])
-
-    # p = Test(basepoint = Vector([Decimal('-0.3010471533'), Decimal('-0.4919135404'), Decimal('0')]), direction_vectors=[Decimal('-0.09084497715'), Decimal('0.5086234347'), 1])
-
-    # print(p)
-
-    t = Test(basepoint = 1, direction_vectors =2)
-
-    print(t)
+# print(matxMultiply(a,b))
 
 
 
+I = [[1,0,2,0],
+     [0,1,0,0],
+     [0,0,1,0],
+     [0,0,0,1]]
 
+# print(transpose(I))
+# print(transpose2(I))
+
+
+# 用于测试
+m1 = [1,1]
+m2 = [2,5]
+l = list(zip(m1,m2))
+# print(l)
+# print([sum(i*j for i, j in l)])
+
+
+A = [[1,1]]
+B = [[2],[5]]
+# 虽然这个计算的结果和下面是一样的，但过程是错误的，多维的情况下会不适应
+# print([[sum(i*j for i, j in zip(a, b)) for a in A] for b in zip(*B)])
+# 正确的方式
+# print([[sum(i*j for i, j in zip(a, b)) for b in zip(*B)] for a in A])
+
+# print(matxMultiply(A,B))
+
+
+# 增广矩阵
+b =[[1],[2],[3],[4]]
+
+l = list( map(list,zip(I,b)) )
+
+# print(l)
+# 正确结果
+print([i+j for i,j in zip(I,b)] )
+
+
+def scaleRow(M, r, scale):
+    if scale ==0:
+        raise ValueError('scale的值不能为零')
+    else:
+        M[r] = [scale * i for i in M[r]]
+
+scaleRow(I,2,5)
+print(I)
